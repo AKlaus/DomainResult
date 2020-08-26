7e1218a5295d98fd93970c7e9695b8fadcbc9484
Alex Klaus <alex.a.klaus@gmail.com>
2020-08-26 21:26:36 +1000

    Added assembly attributes:  - CLSCompliant(true)	// Ensure it can be used by any other .NET language  - ComVisible(false)	// No need in COM exposure (it's true by default)

---
+++ src/Mvc/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]	// Ensure it can be used by any other .NET language
+[assembly: ComVisible(false)]	// No need in COM exposure (it's true by default)
