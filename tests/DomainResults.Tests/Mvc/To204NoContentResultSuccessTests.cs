d399686bf3dae48693515cfd2cc9535db6aeea56
Alex Klaus <alex.a.klaus@gmail.com>
2020-09-05 14:55:44 +1000

    Added 2 tests for NoContentResults for ActionResults<T>

--- tests/DomainResults.Tests/Mvc/To204NoContentResultSuccessTests.cs
+++ tests/DomainResults.Tests/Mvc/To204NoContentResultSuccessTests.cs
@@ -35,5 +35,33 @@ namespace DomainResults.Mvc.Tests
 			// THEN the response type is correct
 			Assert.IsType<NoContentResult>(actionRes);
 		}
+
+#if !NETCOREAPP2_0
+		[Fact]
+		public void DomainResultConverted_To_NoContentOfT()
+		{
+			// GIVEN a successful domain result
+			var domainRes = DomainResult.Success();
+
+			// WHEN convert it to ActionResult
+			var actionRes = domainRes.ToActionResultOfT();
+
+			// THEN the response type is correct
+			Assert.IsType<NoContentResult>(actionRes.Result);
+		}
+
+		[Fact]
+		public async Task DomainResult_Task_Converted_To_NoContentOfT()
+		{
+			// GIVEN a successful domain result
+			var domainRes = DomainResult.SuccessTask();
+
+			// WHEN convert it to ActionResult
+			var actionRes = await domainRes.ToActionResultOfT();
+
+			// THEN the response type is correct
+			Assert.IsType<NoContentResult>(actionRes.Result);
+		}
+#endif
 	}
 }
