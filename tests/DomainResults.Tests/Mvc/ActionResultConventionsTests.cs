0db0419df7b7662a8ba9534ab1d97fda5c5854f9
Alex Klaus <alex.a.klaus@gmail.com>
2022-05-23 21:32:30 +1000

    Added tests for `Conflict` state

--- tests/DomainResults.Tests/Mvc/ActionResultConventionsTests.cs
+++ tests/DomainResults.Tests/Mvc/ActionResultConventionsTests.cs
@@ -7,96 +7,95 @@ using Microsoft.AspNetCore.Mvc;
 
 using Xunit;
 
-namespace DomainResults.Tests.Mvc
+namespace DomainResults.Tests.Mvc;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[Collection("Sequential")]
+public class ActionResult_Conventions_Tests
 {
-	[SuppressMessage("ReSharper", "InconsistentNaming")]
-	[Collection("Sequential")]
-	public class ActionResult_Conventions_Tests
+	[Theory]
+	[InlineData(null, 400)]
+	[InlineData(422,  422)]
+	public void FailedHttpCode_Is_Honoured_in_Error_Response_Test(int? failedHttpCode, int expectedFailedHttpCode)
 	{
-		[Theory]
-		[InlineData(null, 400)]
-		[InlineData(422,  422)]
-		public void FailedHttpCode_Is_Honoured_in_Error_Response_Test(int? failedHttpCode, int expectedFailedHttpCode)
-		{
-			var defaultValue = ActionResultConventions.FailedHttpCode;
+		var defaultValue = ActionResultConventions.FailedHttpCode;
 
-			// GIVEN a custom HTTP code for errors
-			if (failedHttpCode.HasValue)
-				ActionResultConventions.FailedHttpCode = failedHttpCode.Value;
+		// GIVEN a custom HTTP code for errors
+		if (failedHttpCode.HasValue)
+			ActionResultConventions.FailedHttpCode = failedHttpCode.Value;
 
-			// WHEN a IDomainResult with Status 'Error' gets converted to ActionResult
-			var domainResult = IDomainResult.Failed();
-			var actionRes = domainResult.ToActionResult() as ObjectResult;
+		// WHEN a IDomainResult with Status 'Error' gets converted to ActionResult
+		var domainResult = IDomainResult.Failed();
+		var actionRes = domainResult.ToActionResult() as ObjectResult;
 
-			// THEN the HTTP code is expected
-			Assert.Equal(expectedFailedHttpCode, actionRes!.StatusCode);
+		// THEN the HTTP code is expected
+		Assert.Equal(expectedFailedHttpCode, actionRes!.StatusCode);
 
-			ActionResultConventions.FailedHttpCode = defaultValue;
-		}
+		ActionResultConventions.FailedHttpCode = defaultValue;
+	}
 
-		[Theory]
-		[InlineData(null, "Bad Request")]
-		[InlineData("1", "1")]
-		public void FailedProblemDetailsTitle_Is_Honoured_in_Error_Response_Test(string failedTitle, string expectedFailedTitle)
-		{
-			var defaultValue = ActionResultConventions.FailedProblemDetailsTitle;
+	[Theory]
+	[InlineData(null, "Bad Request")]
+	[InlineData("1", "1")]
+	public void FailedProblemDetailsTitle_Is_Honoured_in_Error_Response_Test(string failedTitle, string expectedFailedTitle)
+	{
+		var defaultValue = ActionResultConventions.FailedProblemDetailsTitle;
 
-			// GIVEN a custom HTTP code for errors
-			if (!string.IsNullOrEmpty(failedTitle))
-				ActionResultConventions.FailedProblemDetailsTitle = failedTitle;
+		// GIVEN a custom HTTP code for errors
+		if (!string.IsNullOrEmpty(failedTitle))
+			ActionResultConventions.FailedProblemDetailsTitle = failedTitle;
 
-			// WHEN a IDomainResult with Status 'Error' gets converted to ActionResult
-			var domainResult = IDomainResult.Failed();
-			var actionRes = domainResult.ToActionResult() as ObjectResult;
-			var problemDetails = actionRes!.Value as ProblemDetails;
+		// WHEN a IDomainResult with Status 'Error' gets converted to ActionResult
+		var domainResult = IDomainResult.Failed();
+		var actionRes = domainResult.ToActionResult() as ObjectResult;
+		var problemDetails = actionRes!.Value as ProblemDetails;
 
-			// THEN the ProblemDetails Title is expected
-			Assert.Equal(expectedFailedTitle, problemDetails!.Title);
+		// THEN the ProblemDetails Title is expected
+		Assert.Equal(expectedFailedTitle, problemDetails!.Title);
 
-			ActionResultConventions.FailedProblemDetailsTitle = defaultValue;
-		}
+		ActionResultConventions.FailedProblemDetailsTitle = defaultValue;
+	}
 
-		[Theory]
-		[InlineData(null, 404)]
-		[InlineData(499, 499)]
-		public void NotFoundHttpCode_Is_Honoured_in_NotFound_Response_Test(int? notFoundHttpCode, int expectedNotFoundHttpCode)
-		{
-			var defaultValue = ActionResultConventions.NotFoundHttpCode;
+	[Theory]
+	[InlineData(null, 404)]
+	[InlineData(499, 499)]
+	public void NotFoundHttpCode_Is_Honoured_in_NotFound_Response_Test(int? notFoundHttpCode, int expectedNotFoundHttpCode)
+	{
+		var defaultValue = ActionResultConventions.NotFoundHttpCode;
 
-			// GIVEN a custom HTTP code for 'Not Found'
-			if (notFoundHttpCode.HasValue)
-				ActionResultConventions.NotFoundHttpCode = notFoundHttpCode.Value;
+		// GIVEN a custom HTTP code for 'Not Found'
+		if (notFoundHttpCode.HasValue)
+			ActionResultConventions.NotFoundHttpCode = notFoundHttpCode.Value;
 
-			// WHEN a IDomainResult with Status 'Not Found' gets converted to ActionResult
-			var domainResult = IDomainResult.NotFound();
-			var actionRes = domainResult.ToActionResult() as ObjectResult;
+		// WHEN a IDomainResult with Status 'Not Found' gets converted to ActionResult
+		var domainResult = IDomainResult.NotFound();
+		var actionRes = domainResult.ToActionResult() as ObjectResult;
 
-			// THEN the HTTP code is expected
-			Assert.Equal(expectedNotFoundHttpCode, actionRes!.StatusCode);
+		// THEN the HTTP code is expected
+		Assert.Equal(expectedNotFoundHttpCode, actionRes!.StatusCode);
 
-			ActionResultConventions.NotFoundHttpCode = defaultValue;
-		}
+		ActionResultConventions.NotFoundHttpCode = defaultValue;
+	}
 
-		[Theory]
-		[InlineData(null, "Not Found")]
-		[InlineData("1", "1")]
-		public void NotFoundHttpDetailsTitle_Is_Honoured_in_NotFound_Response_Test(string notFoundTitle, string expectedNotFoundTitle)
-		{
-			var defaultValue = ActionResultConventions.NotFoundProblemDetailsTitle;
+	[Theory]
+	[InlineData(null, "Not Found")]
+	[InlineData("1", "1")]
+	public void NotFoundHttpDetailsTitle_Is_Honoured_in_NotFound_Response_Test(string notFoundTitle, string expectedNotFoundTitle)
+	{
+		var defaultValue = ActionResultConventions.NotFoundProblemDetailsTitle;
 
-			// GIVEN a custom HTTP code for errors
-			if (!string.IsNullOrEmpty(notFoundTitle))
-				ActionResultConventions.NotFoundProblemDetailsTitle = notFoundTitle;
+		// GIVEN a custom HTTP code for errors
+		if (!string.IsNullOrEmpty(notFoundTitle))
+			ActionResultConventions.NotFoundProblemDetailsTitle = notFoundTitle;
 
-			// WHEN a IDomainResult with Status 'Not Found' gets converted to ActionResult
-			var domainResult = IDomainResult.NotFound();
-			var actionRes = domainResult.ToActionResult() as ObjectResult;
-			var problemDetails = actionRes!.Value as ProblemDetails;
+		// WHEN a IDomainResult with Status 'Not Found' gets converted to ActionResult
+		var domainResult = IDomainResult.NotFound();
+		var actionRes = domainResult.ToActionResult() as ObjectResult;
+		var problemDetails = actionRes!.Value as ProblemDetails;
 
-			// THEN the ProblemDetails Title is expected
-			Assert.Equal(expectedNotFoundTitle, problemDetails!.Title);
+		// THEN the ProblemDetails Title is expected
+		Assert.Equal(expectedNotFoundTitle, problemDetails!.Title);
 
-			ActionResultConventions.NotFoundProblemDetailsTitle = defaultValue;
-		}
+		ActionResultConventions.NotFoundProblemDetailsTitle = defaultValue;
 	}
 }
--- tests/DomainResults.Tests/Mvc/To4xxActionResultTests.cs
+++ tests/DomainResults.Tests/Mvc/To4xxActionResultTests.cs
@@ -7,126 +7,135 @@ using DomainResults.Mvc;
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
-namespace DomainResults.Tests.Mvc
+namespace DomainResults.Tests.Mvc;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[Collection("Sequential")]
+public class To_4xx_ActionResult_Tests
 {
-	[SuppressMessage("ReSharper", "InconsistentNaming")]
-	[Collection("Sequential")]
-	public class To_4xx_ActionResult_Tests
+	[Theory]
+	[MemberData(nameof(FailedResultWithValueCases))]
+	public void Failed_DomainResult_With_Value<TValue>(IDomainResult<TValue> domainValue, int expectedCode, string expectedTitle, string expectedErrorMsg)
 	{
-		[Theory]
-		[MemberData(nameof(FailedResultWithValueCases))]
-		public void Failed_DomainResult_With_Value<TValue>(IDomainResult<TValue> domainValue, int expectedCode, string expectedTitle, string expectedErrorMsg)
-		{
-			var actionRes = domainValue.ToActionResult();
+		var actionRes = domainValue.ToActionResult();
 
-			Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
-		}		
-		public static readonly IEnumerable<object[]> FailedResultWithValueCases = TestCases(DomainResult.Failed<int>, DomainResult.NotFound<int>, DomainResult.Unauthorized<int>);
+		Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
+	}		
+	public static readonly IEnumerable<object[]> FailedResultWithValueCases = TestCases(DomainResult.Failed<int>, DomainResult.NotFound<int>, DomainResult.Unauthorized<int>, DomainResult.Conflict<int>, DomainResult.CriticalDependencyError<int>);
 
-		[Theory]
-		[MemberData(nameof(FailedResultWithValueTaskCases))]
-		public async Task Failed_DomainResult_With_Value_Task<TValue>(Task<IDomainResult<TValue>> domainValueTask, int expectedCode, string expectedTitle, string expectedErrorMsg)
-		{
-			var actionRes = await domainValueTask.ToActionResult();
+	[Theory]
+	[MemberData(nameof(FailedResultWithValueTaskCases))]
+	public async Task Failed_DomainResult_With_Value_Task<TValue>(Task<IDomainResult<TValue>> domainValueTask, int expectedCode, string expectedTitle, string expectedErrorMsg)
+	{
+		var actionRes = await domainValueTask.ToActionResult();
 
-			Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
-		}
-		public static readonly IEnumerable<object[]> FailedResultWithValueTaskCases = TestCases(DomainResult.FailedTask<int>, DomainResult.NotFoundTask<int>, DomainResult.UnauthorizedTask<int>);
-
-		[Theory]
-		[MemberData(nameof(FailedResultCases))]
-		public void Failed_DomainResult(IDomainResult domainValue, int expectedCode, string expectedTitle, string expectedErrorMsg)
-		{
-			var actionRes = domainValue.ToActionResult();
-
-			Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
-		}
-		public static readonly IEnumerable<object[]> FailedResultCases = TestCases(DomainResult.Failed, DomainResult.NotFound, DomainResult.Unauthorized);
-
-		[Theory]
-		[MemberData(nameof(FailedResultTaskCases))]
-		public async Task Failed_DomainResult_Task(Task<IDomainResult> domainValueTask, int expectedCode, string expectedTitle, string expectedErrorMsg)
-		{
-			var actionRes = await domainValueTask.ToActionResult();
-
-			Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
-		}
-		public static readonly IEnumerable<object[]> FailedResultTaskCases = TestCases(DomainResult.FailedTask, DomainResult.NotFoundTask, DomainResult.UnauthorizedTask);
-
-		[Theory]
-		[MemberData(nameof(FailedValueTestCases))]
-		public void Failed_ValueResult((int, IDomainResult) domainValue, int expectedCode, string expectedTitle, string expectedErrorMsg)
-		{
-			var actionRes = domainValue.ToActionResult();
-
-			Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
-		}
-		public static readonly IEnumerable<object[]> FailedValueTestCases = TestCases(DomainResult.Failed, DomainResult.NotFound, DomainResult.Unauthorized, v => (10, v));
-
-		[Theory]
-		[MemberData(nameof(FailedValueTaskTestCases))]
-		public async Task Failed_ValueResult_Task(Task<(int, IDomainResult)> domainValueTask, int expectedCode, string expectedTitle, string expectedErrorMsg)
-		{
-			var actionRes = await domainValueTask.ToActionResult();
-
-			Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
-		}
-		[Theory]
-		[MemberData(nameof(FailedValueTaskTestCases))]
-		public void Failed_ValueResult_Task_ActionOfT(Task<(int, IDomainResult)> domainValueTask, int expectedCode, string expectedTitle, string expectedErrorMsg)
-		{
-			var actionResTask = domainValueTask.ToActionResult();
-
-			Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionResTask.Result, expectedCode, expectedTitle, expectedErrorMsg);
-		}
-		public static readonly IEnumerable<object[]> FailedValueTaskTestCases = TestCases(DomainResult.Failed, DomainResult.NotFound, DomainResult.Unauthorized, v => Task.FromResult((10, v)));
-
-		#region Auxiliary methods [PRIVATE] -----------------------------------
-
-		/// <summary>
-		///		The 'THEN' section of the tests, checking the Response type and the <see cref="ProblemDetails"/>
-		/// </summary>
-		/// <param name="actionResult"> The <see cref="IActionResult"/> in question </param>
-		/// <param name="expectedCode"> The expected HTTP code </param>
-		/// <param name="expectedTitle"> The expected title in the <see cref="ProblemDetails"/> </param>
-		/// <param name="expectedErrorMsg"> The expected description messages in the <see cref="ProblemDetails"/> </param>
-		private void Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(IActionResult actionResult, int expectedCode, string expectedTitle, string expectedErrorMsg)
-		{
-			// THEN the response type is correct
-			var objResult = actionResult as ObjectResult;
-			Assert.NotNull(objResult);
-			var problemDetails = objResult!.Value as ProblemDetails;
-			Assert.NotNull(problemDetails);
-
-			// and the ProblemDetails properties are as expected
-			Assert.Equal(expectedCode, problemDetails!.Status);
-			Assert.Equal(expectedTitle, problemDetails.Title);
-			Assert.Equal(expectedErrorMsg, problemDetails.Detail);
-		}
-
-		/// <summary>
-		///		Get test cases (input test parameters for the <see cref="MemberDataAttribute"/>)
-		/// </summary>
-		/// <param name="domainErrorFunc"> The 'Error' function </param>
-		/// <param name="domainNotFoundFunc"> The 'NotFound' function </param>
-		/// <param name="domainUnauthFunc"> The 'Unauthorized' function </param>
-		/// <param name="wrapInFunc"> Optional wrapper <see cref="ValueTuple"/> of the Domain result method </param>
-		/// <returns> Input test parameters </returns>
-		private static IEnumerable<object[]> TestCases<T>(Func<IEnumerable<string>,T> domainErrorFunc, Func<IEnumerable<string>, T> domainNotFoundFunc, Func<string, T> domainUnauthFunc, Func<T, object>? wrapInFunc = null)
-		{
-			object OptionalWrapper (T value) => wrapInFunc?.Invoke(value) ?? value!;
-
-			var returnValues = new List<object[]>
-				{
-					new[] { OptionalWrapper(domainErrorFunc(new[] { "1" })),		ActionResultConventions.FailedHttpCode,	 "Bad Request", "1" },
-					new[] { OptionalWrapper(domainErrorFunc(new[] { "1", "2" })),	ActionResultConventions.FailedHttpCode,	 "Bad Request", "1, 2" },
-					new[] { OptionalWrapper(domainNotFoundFunc(new[] { "1" })),		ActionResultConventions.NotFoundHttpCode, "Not Found",   "1" },
-					new[] { OptionalWrapper(domainNotFoundFunc(new[] { "1", "2" })),ActionResultConventions.NotFoundHttpCode, "Not Found",   "1, 2" },
-					new[] { OptionalWrapper(domainUnauthFunc("1")),					ActionResultConventions.UnauthorizedHttpCode, "Unauthorized access",   "1" },
-				};
-			foreach (var val in returnValues)
-				yield return val;
-		}
-		#endregion // Auxiliary methods [PRIVATE] -----------------------------
+		Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
 	}
+	public static readonly IEnumerable<object[]> FailedResultWithValueTaskCases = TestCases(DomainResult.FailedTask<int>, DomainResult.NotFoundTask<int>, DomainResult.UnauthorizedTask<int>, DomainResult.ConflictTask<int>, DomainResult.CriticalDependencyErrorTask<int>);
+
+	[Theory]
+	[MemberData(nameof(FailedResultCases))]
+	public void Failed_DomainResult(IDomainResult domainValue, int expectedCode, string expectedTitle, string expectedErrorMsg)
+	{
+		var actionRes = domainValue.ToActionResult();
+
+		Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
+	}
+	public static readonly IEnumerable<object[]> FailedResultCases = TestCases(DomainResult.Failed, DomainResult.NotFound, DomainResult.Unauthorized, DomainResult.Conflict, DomainResult.CriticalDependencyError);
+
+	[Theory]
+	[MemberData(nameof(FailedResultTaskCases))]
+	public async Task Failed_DomainResult_Task(Task<IDomainResult> domainValueTask, int expectedCode, string expectedTitle, string expectedErrorMsg)
+	{
+		var actionRes = await domainValueTask.ToActionResult();
+
+		Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
+	}
+	public static readonly IEnumerable<object[]> FailedResultTaskCases = TestCases(DomainResult.FailedTask, DomainResult.NotFoundTask, DomainResult.UnauthorizedTask, DomainResult.ConflictTask, DomainResult.CriticalDependencyErrorTask);
+
+	[Theory]
+	[MemberData(nameof(FailedValueTestCases))]
+	public void Failed_ValueResult((int, IDomainResult) domainValue, int expectedCode, string expectedTitle, string expectedErrorMsg)
+	{
+		var actionRes = domainValue.ToActionResult();
+
+		Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
+	}
+	public static readonly IEnumerable<object[]> FailedValueTestCases = TestCases(DomainResult.Failed, DomainResult.NotFound, DomainResult.Unauthorized, DomainResult.Conflict, DomainResult.CriticalDependencyError, v => (10, v));
+
+	[Theory]
+	[MemberData(nameof(FailedValueTaskTestCases))]
+	public async Task Failed_ValueResult_Task(Task<(int, IDomainResult)> domainValueTask, int expectedCode, string expectedTitle, string expectedErrorMsg)
+	{
+		var actionRes = await domainValueTask.ToActionResult();
+
+		Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionRes, expectedCode, expectedTitle, expectedErrorMsg);
+	}
+	[Theory]
+	[MemberData(nameof(FailedValueTaskTestCases))]
+	public void Failed_ValueResult_Task_ActionOfT(Task<(int, IDomainResult)> domainValueTask, int expectedCode, string expectedTitle, string expectedErrorMsg)
+	{
+		var actionResTask = domainValueTask.ToActionResult();
+
+		Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(actionResTask.Result, expectedCode, expectedTitle, expectedErrorMsg);
+	}
+	public static readonly IEnumerable<object[]> FailedValueTaskTestCases = TestCases(DomainResult.Failed, DomainResult.NotFound, DomainResult.Unauthorized, DomainResult.Conflict, DomainResult.CriticalDependencyError, v => Task.FromResult((10, v)));
+
+	#region Auxiliary methods [PRIVATE] -----------------------------------
+
+	/// <summary>
+	///		The 'THEN' section of the tests, checking the Response type and the <see cref="ProblemDetails"/>
+	/// </summary>
+	/// <param name="actionResult"> The <see cref="IActionResult"/> in question </param>
+	/// <param name="expectedCode"> The expected HTTP code </param>
+	/// <param name="expectedTitle"> The expected title in the <see cref="ProblemDetails"/> </param>
+	/// <param name="expectedErrorMsg"> The expected description messages in the <see cref="ProblemDetails"/> </param>
+	private void Then_ResponseType_Correct_And_ProblemDetails_StatusAndText_Correct(IActionResult actionResult, int expectedCode, string expectedTitle, string expectedErrorMsg)
+	{
+		// THEN the response type is correct
+		var objResult = actionResult as ObjectResult;
+		Assert.NotNull(objResult);
+		var problemDetails = objResult!.Value as ProblemDetails;
+		Assert.NotNull(problemDetails);
+
+		// and the ProblemDetails properties are as expected
+		Assert.Equal(expectedCode, problemDetails!.Status);
+		Assert.Equal(expectedTitle, problemDetails.Title);
+		Assert.Equal(expectedErrorMsg, problemDetails.Detail);
+	}
+
+	/// <summary>
+	///		Get test cases (input test parameters for the <see cref="MemberDataAttribute"/>)
+	/// </summary>
+	/// <param name="domainFailedFunc"> The <see cref="DomainOperationStatus.Failed"/> function </param>
+	/// <param name="domainNotFoundFunc"> The <see cref="DomainOperationStatus.NotFound"/> function </param>
+	/// <param name="domainUnauthFunc"> The <see cref="DomainOperationStatus.Unauthorized"/> function </param>
+	/// <param name="domainConflictFunc"> The <see cref="DomainOperationStatus.Conflict"/> function </param>
+	/// <param name="domainCriticalFunc"> The <see cref="DomainOperationStatus.CriticalDependencyError"/> function </param>
+	/// <param name="wrapInFunc"> Optional wrapper <see cref="ValueTuple"/> of the Domain result method </param>
+	/// <returns> Input test parameters </returns>
+	private static IEnumerable<object[]> TestCases<T>(
+		Func<IEnumerable<string>,T> domainFailedFunc, 
+		Func<IEnumerable<string>, T> domainNotFoundFunc, 
+		Func<string, T> domainUnauthFunc,  
+		Func<string, T> domainConflictFunc,  
+		Func<string, T> domainCriticalFunc, 
+		Func<T, object>? wrapInFunc = null)
+	{
+		object OptionalWrapper (T value) => wrapInFunc?.Invoke(value) ?? value!;
+
+		var returnValues = new List<object[]>
+			{
+				new[] { OptionalWrapper(domainFailedFunc(new[] { "1" })),		ActionResultConventions.FailedHttpCode,	 "Bad Request", "1" },
+				new[] { OptionalWrapper(domainFailedFunc(new[] { "1", "2" })),	ActionResultConventions.FailedHttpCode,	 "Bad Request", "1, 2" },
+				new[] { OptionalWrapper(domainNotFoundFunc(new[] { "1" })),		ActionResultConventions.NotFoundHttpCode, "Not Found",   "1" },
+				new[] { OptionalWrapper(domainNotFoundFunc(new[] { "1", "2" })),ActionResultConventions.NotFoundHttpCode, "Not Found",   "1, 2" },
+				new[] { OptionalWrapper(domainUnauthFunc("1")),					ActionResultConventions.UnauthorizedHttpCode, "Unauthorized access",   "1" },
+				new[] { OptionalWrapper(domainConflictFunc("1")),				ActionResultConventions.ConflictHttpCode, "Conflict with the current state",   "1" },
+				new[] { OptionalWrapper(domainCriticalFunc("1")),				ActionResultConventions.CriticalDependencyErrorHttpCode, "External service unavailable",   "1" },
+			};
+		foreach (var val in returnValues)
+			yield return val;
+	}
+	#endregion // Auxiliary methods [PRIVATE] -----------------------------
 }
