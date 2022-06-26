98405c423e28b64e7d42846035b7d8f36aa1f54c
Alex Klaus <alex.a.klaus@gmail.com>
2022-06-26 20:59:46 +1000

    Added samples for 400 code

---
+++ samples/WebApiMinimal/Routes/400BadRequestCustomErrorResponses.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+
+using DomainResults.Examples.Domain;
+using DomainResults.Mvc;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace DomainResults.Examples.WebApiMinimal.Routes;
+
+internal static partial class RoutesExtension 
+{
+	/// <summary>
+	///		Map all routes to return HTTP 400 status with custom error message
+	/// </summary>
+	public static void MapBadRequestCustomErrorResponses(this IEndpointRouteBuilder app)
+	{
+		DomainFailedService service = new();
+		
+		var routes = new [] 
+			{
+				app.MapGet("GetErrorWithCustomStatusAndMessage", 
+					   () => service.GetFailedWithNoMessage()
+											.ToResult((problemDetails, state) =>
+											{
+												if (state.Errors.Any())
+													return;
+												problemDetails.Status = 422;
+												problemDetails.Title = "D'oh!";
+												problemDetails.Detail = "I wish devs put more efforts into it...";
+											}))
+				   .Produces(StatusCodes.Status422UnprocessableEntity),
+				
+				app.MapGet("GetErrorWithCustomTitleAndOriginalMessage",
+					   () => service.GetFailedWithMessageTask()
+											.ToResult((problemDetails, _) => { problemDetails.Title = "D'oh!"; })
+						  )
+				   .Produces(StatusCodes.Status400BadRequest),
+				
+				app.MapGet("GetErrorWithCustomStatusAndMessageWhenExpectedNumber", 
+					() => service.GetFailedWithNoMessageWhenExpectedNumber()
+										.ToResult((problemDetails, state) =>
+										{
+											if (state.Errors.Any())
+												return;
+											problemDetails.Status = 422;
+											problemDetails.Title = "D'oh!";
+											problemDetails.Detail = "I wish devs put more efforts into it...";
+										})
+						  )
+				   .Produces(StatusCodes.Status422UnprocessableEntity),
+				
+				app.MapGet("GetErrorWithCustomTitleAndOriginalMessageWhenExpectedNumber", 
+					() => service.GetFailedWithMessageWhenExpectedNumberTask()
+										.ToResult((problemDetails, _) => { problemDetails.Title = "D'oh!"; })
+						  )
+				   .Produces(StatusCodes.Status400BadRequest),
+				
+				app.MapGet("GetErrorWithCustomStatusAndMessageWhenExpectedNumberAsTuple", 
+					() => service.GetFailedWithNoMessageWhenExpectedNumberTuple()
+										.ToResult((problemDetails, state) =>
+										{
+											if (state.Errors.Any())
+												return;
+											problemDetails.Status = 422;
+											problemDetails.Title = "D'oh!";
+											problemDetails.Detail = "I wish devs put more efforts into it...";
+										})
+						  )
+				   .Produces(StatusCodes.Status422UnprocessableEntity),
+				
+				app.MapGet("GetErrorWithCustomTitleAndOriginalMessageWhenExpectedNumberAsTuple", 
+					() => service.GetFailedWithMessageWhenExpectedNumberTupleTask()
+										.ToResult((problemDetails, _) => { problemDetails.Title = "D'oh!"; })
+						  )
+				   .Produces(StatusCodes.Status400BadRequest),
+				
+				app.MapGet("GetErrorOfTWithCustomTitleAndOriginalMessageWhenExpectedNumberAsTuple", 
+					() => service.GetFailedWithMessageWhenExpectedNumberTupleTask()
+										.ToResult((problemDetails, _) => { problemDetails.Title = "D'oh!"; })
+						  )
+				   .Produces(StatusCodes.Status400BadRequest)
+			};
+		
+		foreach (var route in routes)                               
+		   route.WithTags("Failed: 400/422 BadRequest with custom message");
+	}
+}
---
+++ samples/WebApiMinimal/Routes/400BadRequestResponses.cs
@@ -0,0 +1,38 @@
+﻿using DomainResults.Examples.Domain;
+using DomainResults.Mvc;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace DomainResults.Examples.WebApiMinimal.Routes;
+
+internal static partial class RoutesExtension 
+{
+	/// <summary>
+	///		Map all routes to return HTTP 400 status
+	/// </summary>
+	public static void MapBadRequestResponses(this IEndpointRouteBuilder app)
+	{
+		DomainFailedService service = new();
+		
+		var routes = new [] 
+			{
+				app.MapGet("GetErrorWithNoMessage", () => service.GetFailedWithNoMessage().ToResult()),
+				app.MapGet("GetErrorWithMessage", () => service.GetFailedWithMessage().ToResult()),
+				app.MapGet("GetErrorWithMessages", () => service.GetFailedWithMessages().ToResult()),
+				
+				app.MapGet("GetErrorWithNoMessageWhenExpectedNumber", () => service.GetFailedWithNoMessageWhenExpectedNumber().ToResult()),
+				app.MapGet("GetFailedWithMessageWhenExpectedNumber", () => service.GetFailedWithMessageWhenExpectedNumber().ToResult()),
+				app.MapGet("GetFailedWithMessagesWhenExpectedNumber", () => service.GetFailedWithMessagesWhenExpectedNumber().ToResult()),
+				
+				app.MapGet("GetFailedWithNoMessageWhenExpectedNumberTuple", () => service.GetFailedWithNoMessageWhenExpectedNumberTuple().ToResult()),
+				app.MapGet("GetFailedWithMessageWhenExpectedNumberTuple", () => service.GetFailedWithMessageWhenExpectedNumberTuple().ToResult()),
+				app.MapGet("GetFailedWithMessagesWhenExpectedNumberTuple", () => service.GetFailedWithMessagesWhenExpectedNumberTuple().ToResult())
+			};
+		
+		foreach (var route in routes)                               
+		   route.WithTags("Failed: 400 BadRequest")
+				.Produces(StatusCodes.Status400BadRequest);
+	}
+}
