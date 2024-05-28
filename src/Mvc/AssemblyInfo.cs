57e5b85edf908d626fcd1498bf086d465eba9b5d
Alex Klaus <alexander.klaus@angloamerican.com>
2024-05-28 11:33:07 +1000

    Fixed typos

--- src/Mvc/AssemblyInfo.cs
+++ src/Mvc/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System;
 using System.Runtime.InteropServices;
 
-[assembly: CLSCompliant(false)]	// Can't be CLS colpiant (supported by other .NET language) as used calss ProblemDetails is not CLS compliant
+[assembly: CLSCompliant(false)]	// Can't be CLS compliant (supported by other .NET language) as used `ProblemDetails` class is not CLS compliant
 [assembly: ComVisible(false)]	// No need in COM exposure (it's true by default)
