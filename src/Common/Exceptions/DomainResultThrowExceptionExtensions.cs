50ceb39df351f4a85aa0ee2d5fa36a6b72213112
Alex Klaus <alexander.klaus@angloamerican.com>
2024-12-22 17:03:01 +1000

    Added extension methods for IDomainResult that throw an exception when !result.IsSuccess

---
+++ src/Common/Exceptions/DomainResultThrowExceptionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+
+namespace DomainResults.Common.Exceptions
+{
+	/// <summary>
+	///     Extension methods to throw exception if <see cref="DomainResult.IsSuccess"/> is <value>false</value>.
+	/// </summary>
+	public static class DomainResultThrowExceptionExtensions
+	{
+		/// <summary>
+		///		Throw <see cref="DomainResultException"/> if <paramref name="domainResult"/>'s <see cref="DomainResult.IsSuccess"/> is <value>false</value> 
+		/// </summary>
+		/// <param name="domainResult"> The IDomainResult to check </param>
+		/// <param name="errMsg"> The error message </param>
+		/// <exception cref="DomainResultException"> The thrown exception if the <see cref="DomainResult.IsSuccess"/> is <value>false</value> </exception>
+		public static void ThrowIfNoSuccess(this IDomainResult domainResult, string? errMsg = null) 
+		{
+			if (!domainResult.IsSuccess)
+				throw new DomainResultException(domainResult, errMsg);
+		}
+		/// <summary>
+		///		Throw <see cref="DomainResultException"/> if <paramref name="domainResult"/>'s <see cref="DomainResult.IsSuccess"/> is <value>false</value> 
+		/// </summary>
+		/// <param name="domainResult"> The IDomainResult to check </param>
+		/// <param name="errMsg"> The error message </param>
+		/// <exception cref="DomainResultException"> The thrown exception if the <see cref="DomainResult.IsSuccess"/> is <value>false</value> </exception>
+		public static void ThrowIfNoSuccess<T>(this IDomainResult<T> domainResult, string? errMsg = null)
+		{
+			if (!domainResult.IsSuccess)
+				throw new DomainResultException(domainResult, errMsg);
+		}
+		
+		/// <summary>
+		///		Throw <see cref="DomainResultException"/> if <paramref name="domainResult"/>'s <see cref="DomainResult.IsSuccess"/> is <value>false</value> 
+		/// </summary>
+		/// <param name="domainResult"> The IDomainResult to check </param>
+		/// <param name="errMsg"> The error message </param>
+		/// <exception cref="DomainResultException"> The thrown exception if the <see cref="DomainResult.IsSuccess"/> is <value>false</value> </exception>
+		public static async void ThrowIfNoSuccess(this Task<IDomainResult> domainResult, string? errMsg = null)
+		{		
+			var dr = await domainResult;
+			if (!dr.IsSuccess)
+				throw new DomainResultException(dr, errMsg);
+		}
+		/// <summary>
+		///		Throw <see cref="DomainResultException"/> if <paramref name="domainResult"/>'s <see cref="DomainResult.IsSuccess"/> is <value>false</value> 
+		/// </summary>
+		/// <param name="domainResult"> The IDomainResult to check </param>
+		/// <param name="errMsg"> The error message </param>
+		/// <exception cref="DomainResultException"> The thrown exception if the <see cref="DomainResult.IsSuccess"/> is <value>false</value> </exception>
+		public static async void ThrowIfNoSuccess<T>(this Task<IDomainResult<T>> domainResult, string? errMsg = null)
+		{
+			var (_, result) = await domainResult;
+			if (!result.IsSuccess)
+				throw new DomainResultException(result, errMsg);
+		}
+	}
+}
---
+++ tests/DomainResults.Tests/Common/DomainResultThrowExceptionExtensionsTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using DomainResults.Common;
+using DomainResults.Common.Exceptions;
+
+using Xunit;
+
+namespace DomainResults.Tests.Common;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public class DomainResult_Throw_Exception_Extensions_Tests
+{
+	[Fact]
+	public void Successful_IDomainResult_Doesnt_Throw_Exception_On_Check()
+	{
+		var domainResult = DomainResult.Success();
+		domainResult.ThrowIfNoSuccess();
+		// No exception thrown
+		Assert.True(domainResult.IsSuccess);
+	}
+	[Fact]
+	public void Successful_IDomainResultOfT_Doesnt_Throw_Exception_On_Check()
+	{
+		var domainResult = DomainResult.Success(10);
+		domainResult.ThrowIfNoSuccess();
+		// No exception thrown
+		Assert.True(domainResult.IsSuccess);
+	}
+	[Fact]
+	public async void Successful_IDomainResult_Task_Doesnt_Throw_Exception_On_Check()
+	{
+		var domainResult = DomainResult.SuccessTask();
+		domainResult.ThrowIfNoSuccess();
+		// No exception thrown
+		Assert.True((await domainResult).IsSuccess);
+	}
+	[Fact]
+	public async void Successful_IDomainResultOfT_Task_Doesnt_Throw_Exception_On_Check()
+	{
+		var domainResult = DomainResult.SuccessTask(10);
+		domainResult.ThrowIfNoSuccess();
+		// No exception thrown
+		Assert.True((await domainResult).IsSuccess);
+	}
+	
+	[Fact]
+	public void Failed_IDomainResult_Throws_Exception_On_Check()
+	{
+		var domainResult = DomainResult.Failed("Bla");
+		var exc = Assert.Throws<DomainResultException>(() => domainResult.ThrowIfNoSuccess("Error Message"));
+		Assert.Equal("Error Message", exc.Message);
+		Assert.Equal(DomainOperationStatus.Failed, exc.DomainResult.Status);
+		Assert.Equal(["Bla"], exc.DomainResult.Errors);
+	}
+	[Fact]
+	public void Failed_IDomainResultOfT_Throws_Exception_On_Check()
+	{
+		var domainResult = DomainResult.Failed<int>("Bla");
+		var exc = Assert.Throws<DomainResultException>(() => domainResult.ThrowIfNoSuccess("Error Message"));
+		Assert.Equal("Error Message", exc.Message);
+		Assert.Equal(DomainOperationStatus.Failed, exc.DomainResult.Status);
+		Assert.Equal(["Bla"], exc.DomainResult.Errors);
+	}
+	[Fact]
+	public void Failed_IDomainResultOfT_Task_Throws_Exception_On_Check()
+	{
+		var domainResult = DomainResult.FailedTask<int>("Bla");
+		var exc = Assert.Throws<DomainResultException>(() => domainResult.ThrowIfNoSuccess("Error Message"));
+		Assert.Equal("Error Message", exc.Message);
+		Assert.Equal(DomainOperationStatus.Failed, exc.DomainResult.Status);
+		Assert.Equal(["Bla"], exc.DomainResult.Errors);
+	}
+}
